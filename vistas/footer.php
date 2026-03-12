61bdd49352245ab25d58d120eb850beb6da49fe6
IsayC <cruzhernandezisay@gmail.com>
2026-03-12 08:03:35 -0600

    Cap 2 - Maquetar plantilla

---
+++ vistas/footer.php
@@ -0,0 +1,7 @@
+<script src="../public/jquery/jquery-3.6.0.min.js"></script>
+    <script src="../public/bootstrap/popper.min.js"></script>
+    <Script src="../public/bootstrap/bootstrap.min.js"></Script>
+
+
+</body>
+</html>
