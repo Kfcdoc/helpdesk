c48a7fe695e915fd8ddcf9d2f9e004baa589a046
IsayC <cruzhernandezisay@gmail.com>
2026-03-17 11:16:27 -0600

    Cap 15- Maquetacion modulo (par1)

--- vistas/asignacion.php
+++ vistas/asignacion.php
@@ -1,17 +1,28 @@
  <?php 
     include "header.php";
     if (isset($_SESSION['usuario'])&& $_SESSION['usuario']['rol'] == 2) {
+        include "../clases/Conexion.php";
+        $con = new Conexion();
+        $conexion = $con->conectar();
+
 ?>
 
 <!-- Page Content -->
     <div class="container">
-    <div class="card border-0 shadow my-5">
-        <div class="card-body p-5">
-        <h1 class="fw-light">Asignación</h1>
-        <p class="lead">Content on the page will</p>
-    </div>
+        <div class="card border-0 shadow my-5">
+            <div class="card-body p-5">
+            <h1 class="fw-light">Asignación de equipos</h1>
+            <p class="lead">
+                <button class="btn btn-primaty" data-toggle="modal" data-target="#modalAsignarEquipo">Asignar Equipo</button>
+                <hr>
+                <div id="tablaAsignacionesLoad"></div>
+            </p>
+            </div>
+        </div>
     </div>  
-<?php include "footer.php";
+<?php 
+    include "asignacion/modalAsignar.php";
+    include "footer.php";
     } else {
         header("Location: ../index.html");
     }
