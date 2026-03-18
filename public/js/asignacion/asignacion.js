61cb6aaf63079bf0a49ffd04075140f918d15dd8
IsayC <cruzhernandezisay@gmail.com>
2026-03-18 16:45:32 -0600

    Cap 17- insert part1

---
+++ public/js/asignacion/asignacion.js
@@ -0,0 +1,20 @@
+
+function asignarEquipo() {
+    $.ajax({
+        type:"POST",
+        data:$('#frmAsignaEquipo').serialize(),
+        url:"../procesos/asignacion/asignar.php",
+        success:function(respuesta) {
+            respuesta = respuesta.trim();
+
+            if (respuesta == 1) {
+                $('#frmAsignaEquipo')[0].reset();
+                Swal.fire(":D","Asignado con exito!","success");
+            } else {
+                Swal.fire(":(","Fallo al asignar!" + respuesta, "error");
+            }
+        }
+    });
+
+    return false;
+}
--- vistas/asignacion.php
+++ vistas/asignacion.php
@@ -23,6 +23,9 @@
 <?php 
     include "asignacion/modalAsignar.php";
     include "footer.php";
+?>
+<script src="../public/js/asignacion/asignacion.js"></script>
+<?php
     } else {
         header("Location: ../index.html");
     }
