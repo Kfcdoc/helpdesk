018dce8393269673c143d9f5f9540c058b6c1140
IsayC <cruzhernandezisay@gmail.com>
2026-03-19 02:08:57 -0600

    Cap-29

--- clases/Reportes.php
+++ clases/Reportes.php
@@ -17,3 +17,49 @@
             return $respuesta;
         }
 
+        public function eliminarReporteCliente($idReporte) {
+            $conexion = parent::conectar();
+            $sql = "DELETE FROM t_reportes WHERE id_reporte = ?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('i', $idReporte);
+            $respuesta = $query->execute();
+            $query->close();
+            return $respuesta;
+        }
+
+        public function obtenerSolucion($idReporte) {
+            $conexion = parent::conectar();
+            $sql = "SELECT solucion_problema, 
+                            estatus
+                    FROM t_reportes 
+                    WHERE id_reporte = '$idReporte'";
+            $respuesta = mysqli_query($conexion, $sql);
+            $reporte = mysqli_fetch_array($respuesta);
+
+            $datos = array(
+                "idReporte" => $idReporte,
+                "estatus" => $reporte['estatus'],
+                "solucion" => $reporte['solucion_problema']
+            );
+
+            return $datos;
+        }
+
+        public function actualizarSolucion($datos) {
+            $conexion = parent::conectar();
+            $sql = "UPDATE t_reportes 
+                    SET id_usuario_tecnico = ?,
+                        solucion_problema = ?,
+                        estatus = ? 
+                    WHERE id_reporte = ?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('isii', $datos['idUsuario'],
+                                        $datos['solucion'],
+                                        $datos['estatus'],
+                                        $datos['idReporte']);
+            $respuesta = $query->execute();
+            $query->close();
+            return $respuesta;
+
+        }
+    }
