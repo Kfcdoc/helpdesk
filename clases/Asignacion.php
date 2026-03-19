96b8349baba6506f243f5993b3e2da8ffb7bde59
IsayC <cruzhernandezisay@gmail.com>
2026-03-18 18:18:31 -0600

    Cap 19-Programacion

--- clases/Asignacion.php
+++ clases/Asignacion.php
@@ -29,5 +29,31 @@
             $query->close();
             return $respuesta;
         }
+        public function eliminarAsignacion($idAsignacion) {
+            $conexion = Conexion::conectar();
+            $sql = "DELETE FROM t_asignacion WHERE id_asignacion = ?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('i', $idAsignacion);
+            $respuesta = $query->execute();
+            $query->close();
+            return $respuesta;
+        }
     }
 ?>
+Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  }
+});
--- public/js/asignacion/asignacion.js
+++ public/js/asignacion/asignacion.js
@@ -1,24 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#tablaAsignacionesLoad').load('asignacion/tablaAsignacion.php');
 });
 
 function asignarEquipo() {
     $.ajax({
-        type:"POST",
-        data:$('#frmAsignaEquipo').serialize(),
-        url:"../procesos/asignacion/asignar.php",
-        success:function(respuesta) {
+        type: "POST",
+        data: $('#frmAsignaEquipo').serialize(),
+        url: "../procesos/asignacion/asignar.php",
+        success: function(respuesta) {
             respuesta = respuesta.trim();
-
             if (respuesta == 1) {
                 $('#frmAsignaEquipo')[0].reset();
                 $('#tablaAsignacionesLoad').load('asignacion/tablaAsignacion.php');
-                Swal.fire(":D","Asignado con exito!","success");
+                Swal.fire(":D", "Asignado con éxito!", "success");
             } else {
-                Swal.fire(":(","Fallo al asignar!" + respuesta, "error");
+                Swal.fire(":(", "Fallo al asignar! " + respuesta, "error");
             }
         }
     });
+    return false;
+}
 
+function eliminarAsignacion(idAsignacion) {
+    Swal.fire({
+        title: '¿Estás seguro de eliminar este registro?',
+        text: "¡Una vez eliminado no podrá ser recuperado!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "POST",
+                data: "idAsignacion=" + idAsignacion,
+                url: "../procesos/asignacion/eliminarAsignacion.php",
+                success: function(respuesta) {
+                    respuesta = respuesta.trim();
+                    if (respuesta == 1) {
+                        $('#tablaAsignacionesLoad').load('asignacion/tablaAsignacion.php');
+                        Swal.fire(":D", "Eliminado con éxito!", "success");
+                    } else {
+                        Swal.fire(":(", "Fallo al eliminar! " + respuesta, "error");
+                    }
+                }
+            });
+        }
+    });
     return false;
 }
