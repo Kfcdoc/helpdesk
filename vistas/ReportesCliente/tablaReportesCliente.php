db4b88e05eb122b68f37be6105954925fb837b4c
IsayC <cruzhernandezisay@gmail.com>
2026-03-18 21:12:51 -0600

    Capp 22

---
+++ vistas/ReportesCliente/tablaReportesCliente.php
@@ -0,0 +1,13 @@
+<table class="table table-sm  dt-responsive nowrap" 
+        style="width:100%" id="tablaReportesClienteDataTable">
+    <thead>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </thead>
+    <tbody>
