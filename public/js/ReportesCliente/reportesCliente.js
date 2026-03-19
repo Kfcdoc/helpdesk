aadec42255c7014d5f9b46f317ae43226e125d15
IsayC <cruzhernandezisay@gmail.com>
2026-03-19 00:27:38 -0600

    Cap 25-Programación modulo reportes

--- public/js/ReportesCliente/reportesCliente.js
+++ public/js/ReportesCliente/reportesCliente.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $('#tablaReporteClienteLoad').load('reportesCliente/tablaReporteCliente.php');
 });
+
+function agregarNuevoReporte() {
+
+    $.ajax({
+        type:"POST",
+        data:$('#frmNuevoReporte').serialize(),
+        url:"../procesos/reportesCliente/agregarNuevoReporte.php",
+        success:function(respuesta) {
+            respuesta = respuesta.trim();
+            if (respuesta == 1) {
+                $('#tablaReporteClienteLoad').load('reportesCliente/tablaReporteCliente.php');
+                $('#frmNuevoReporte')[0].reset();
+                Swal.fire(":D","Agregado con exito!","success");
+            } else {
+                Swal.fire(":(","Error al agregar! " + respuesta, "error");
+            }
+        }
+    });
+
+    return false;
+}
